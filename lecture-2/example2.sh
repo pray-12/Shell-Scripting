bb9f81a06291bfe246d15b5edce7c14d9ccd2f27
Preethy Ray <pray122001@gmail.com>
2025-11-24 10:05:39 +0530

    Update example2.sh

--- lecture-2/example2.sh
+++ lecture-2/example2.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 echo "Enter Two numbers"
-read num1 read num2
-#Multiple operations sum=$((num1+num2)) diff=$((num1-num2) product=$((num1*num2)) division=$((num1/num2))
+read num1 
+read num2
+#Multiple operations 
+sum=$((num1+num2)) 
+diff=$((num1-num2)
+product=$((num1*num2)) 
+division=$((num1/num2))
 #Display results
 echo "Sum is $sum"
-echo "Difference is $diff" echo "Product is $product" echo "Division is $division"
+echo "Difference is $diff"
+echo "Product is $product" 
+echo "Division is $division"
